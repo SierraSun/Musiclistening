39fca3545cd41e2c85956950419e6ad57cdc72c9
sulygirl <31868287+sulygirl@users.noreply.github.com>
2018-11-07 00:14:50 -0500

    camera

--- js/visualization/3d.js
+++ js/visualization/3d.js
@@ -7,11 +7,12 @@ function preload(){
 }
 
 function setup(){
-  createCanvas(1000,500,WEBGL);
+  createCanvas(1000,600,WEBGL);
   // capture = createCapture(VIDEO);
   // capture.size(320,240);
   // capture.hide();
 }
+
 
 function draw(){
   let dx=mouseX-width/2;
@@ -19,26 +20,37 @@ function draw(){
   let v= createVector(dx,dy,0);
   v.div(100);
 
+  let camX = map(mouseX,0,width,-100,0);
+
+  camera(camX,0,(height/2)/tan(PI/6),camX,0,0,0,1,0);
+
+  let fov = map(mouseX,0,width,0,PI);
+  let cameraZ = (height/2.0) / tan(fov/2.0);
+  perspective(fov, width/height, cameraZ/10.0, -100);
+
   directionalLight(255,0,0,v);
   ambientLight(255);
 
-  background(255);
-
-  noStroke();
+  background(0);
 
   rectMode(CENTER);
 
+  for (let x=-200; x<200; x+=50){
+  push();
   rotateY(angle);
   rotateX(angle*0.3);
-  rotateZ(angle*0.2)
+  rotateZ(angle*0.3);
 
+  translate(mouseX/2,0,0);
+  noStroke();
   texture(singer);
   // ambientMaterial(255);
   // rect(0,0,150,150);
-  translate(0,0,mouseX-width/2);
-  sphere(100);
+  sphere(50);
+  pop();
+}
+  translate(0,100);
 
-
-  angle += 0.07;
+  angle += 0.01;
 
 }
